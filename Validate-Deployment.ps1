2e14b291de30682f39a424a47463d1cf3d67b693
System Administrator <admin@mngenv019702.onmicrosoft.com>
2024-11-07 11:03:55 -0500

    validate deployment

--- Validate-Deployment.ps1
+++ Validate-Deployment.ps1
@@ -4,17 +4,13 @@ param (
     [Parameter()]
     [string]$deploymentName = "deploy-rg-fnapp-$nameSuffix",
     [Parameter()]
-    [string]$resourceGroupName = "rg-fnapp-$nameSuffix",
-    [Parameter()]
-    [string]
-    $subscriptionId = "64c3d212-40ed-4c6d-a825-6adfbdf25dad"
+    [string]$resourceGroupName = "rg-fnapp-$nameSuffix"
 )
 
 # echo parameters
 Write-Host "deploymentName: $deploymentName"
 Write-Host "nameSuffix: $nameSuffix"
 Write-Host "resourceGroupName: $resourceGroupName"
-Write-Host "subscriptionId: $subscriptionId"
 
 #az account show                      
 # get functionAppName
@@ -35,6 +31,6 @@ Write-Host "functionKey: $functionKey"
 $testUrl = "https://$functionAppName.azurewebsites.net/api/HttpExample?code=$functionKey"
 Write-Host "testUrl: $testUrl"
 
-# # test function
-# $testResult = Invoke-RestMethod -Uri $testUrl
-# Write-Host "testResult: $testResult"
+# test function
+$testResult = Invoke-RestMethod -Uri $testUrl
+Write-Host "testResult: $testResult"
